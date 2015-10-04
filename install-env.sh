2b12267b2daebe54e2e4836df30f8579b7066f36
thanusha chandrahasa <tchandr1@hawk.iit.edu>
2015-10-04 16:58:22 -0500

    add index.html in shell script

--- install-env.sh
+++ install-env.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y apache2 git
 git clone https://github.com/tchandr1/ApplicationSetup-itmo-544-444-fall2015.git
 
 mv ./ApplicationSetup-itmo-544-444-fall2015/images /var/www/html/images
-mv ./ApplicationSetup-itmo-544-444-fall2015/page1.html /var/www/html/
+mv ./ApplicationSetup-itmo-544-444-fall2015/index.html /var/www/html/
 mv ./ApplicationSetup-itmo-544-444-fall2015/page2.html /var/www/html/
 
 echo "Hello! I am Thanusha Chandrahasa. Course:ITMO544-444, Week05Assignment" > /tmp/hello.txt
