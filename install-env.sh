e49720ec22e8f18331539bc0528d227445fe27c8
thanusha chandrahasa <tchandr1@hawk.iit.edu>
2015-10-04 16:31:12 -0500

    shell script updated

--- install-env.sh
+++ install-env.sh
@@ -3,10 +3,10 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
-git clone https://github.com/tchandr1/EnvironmentalSetup-itmo-544-444-fall2015.git
+git clone https://github.com/tchandr1/ApplicationSetup-itmo-544-444-fall2015.git
 
-mv ./EnvironmentalSetup-itmo-544-444-fall2015/images /var/www/html/images
-mv ./EnvironmentalSetup-itmo-544-444-fall2015/index.html /var/www/html/
+mv ./ApplicationSetup-itmo-544-444-fall2015/images /var/www/html/images
+mv ./ApplicationSetup-itmo-544-444-fall2015/page1.html /var/www/html/
+mv ./ApplicationSetup-itmo-544-444-fall2015/page2.html /var/www/html/
 
 echo "Hello! I am Thanusha Chandrahasa. Course:ITMO544-444, Week05Assignment" > /tmp/hello.txt
-
